408a402f01d32cab2325751ad2b5879b1d1f7e10
Sera Park <naseraya@gmail.com>
2023-06-03 16:57:55 +1000

    add button / search bar updated

--- public/script.js
+++ public/script.js
@@ -1,4 +1,24 @@
 function showSearchBar() {
-    var searchContainer = document.getElementById("search-container");
+    var searchContainer = document.getElementById("autoComplete_wrapper");
     searchContainer.classList.toggle("hidden");
+
+// Clicking on Add button opens search bar/clicking outside of search bar removes search bar
+
+    if (!autoComplete_wrapper.classList.contains("hidden")){
+        document.addEventListener("click", handleOutsideClick);
+    } else {
+        document.removeEventListener("click", handleOutsideClick);
+    }
 }
+
+function handleOutsideClick(event) {
+    var searchContainer = document.getElementById("autoComplete_wrapper");
+    var addButton = document.getElementById("add_button");
+    var targetElement = event.target;
+  
+    // Check if the click target is outside of the search container and search button
+    if (!searchContainer.contains(targetElement) && !addButton.contains(targetElement)) {
+      searchContainer.classList.add("hidden");
+      document.removeEventListener("click", handleOutsideClick);
+    }
+  }
