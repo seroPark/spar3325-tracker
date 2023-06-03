4a7ba165cf1b15ed7a1d89e68222cd9ed757a37b
Sera Park <naseraya@gmail.com>
2023-06-03 17:37:22 +1000

    add button click opens add manually window

--- public/script.js
+++ public/script.js
@@ -1,24 +1,50 @@
-function showSearchBar() {
-    var searchContainer = document.getElementById("autoComplete_wrapper");
-    searchContainer.classList.toggle("hidden");
+// function showSearchBar() {
+//     var searchContainer = document.getElementById("autoComplete_wrapper");
+//     searchContainer.classList.toggle("hidden");
 
-// Clicking on Add button opens search bar/clicking outside of search bar removes search bar
+// // Clicking on Add button opens search bar/clicking outside of search bar removes search bar
 
-    if (!autoComplete_wrapper.classList.contains("hidden")){
-        document.addEventListener("click", handleOutsideClick);
-    } else {
-        document.removeEventListener("click", handleOutsideClick);
+//     if (!autoComplete_wrapper.classList.contains("hidden")){
+//         document.addEventListener("click", handleOutsideClick);
+//     } else {
+//         document.removeEventListener("click", handleOutsideClick);
+//     }
+// }
+
+function showAddMan() {
+        var addManContainer = document.getElementById("addMan_container");
+        addManContainer.classList.toggle("hidden");
+    
+    // Clicking on Add button opens add manually window/clicking outside of window removes window
+    
+        if (!addMan_container.classList.contains("hidden")){
+            document.addEventListener("click", handleOutsideClick);
+        } else {
+            document.removeEventListener("click", handleOutsideClick);
+        }
     }
-}
 
-function handleOutsideClick(event) {
-    var searchContainer = document.getElementById("autoComplete_wrapper");
+
+// function handleOutsideClick(event) {
+//     var searchContainer = document.getElementById("autoComplete_wrapper");
+//     var addButton = document.getElementById("add_button");
+//     var targetElement = event.target;
+  
+//     // Check if the click target is outside of the search container and search button
+//     if (!searchContainer.contains(targetElement) && !addButton.contains(targetElement)) {
+//       searchContainer.classList.add("hidden");
+//       document.removeEventListener("click", handleOutsideClick);
+//     }
+//   }
+
+  function handleOutsideClick(event) {
+    var addManWindow = document.getElementById("addMan_container");
     var addButton = document.getElementById("add_button");
     var targetElement = event.target;
   
     // Check if the click target is outside of the search container and search button
-    if (!searchContainer.contains(targetElement) && !addButton.contains(targetElement)) {
-      searchContainer.classList.add("hidden");
-      document.removeEventListener("click", handleOutsideClick);
+    if (!addManWindow.contains(targetElement) && !addButton.contains(targetElement)) {
+        addManWindow.classList.add("hidden");
+        document.removeEventListener("click", handleOutsideClick);
     }
   }
