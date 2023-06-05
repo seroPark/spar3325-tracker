3e62da7a23bdf0dbdcb9fefb5711231f6e73adad
Sera Park <naseraya@gmail.com>
2023-06-05 12:44:06 +1000

    auto complete fixed

--- public/script.js
+++ public/script.js
@@ -25,81 +25,102 @@ function handleOutsideClick(event) {
 
 
   // The autoComplete.js Engine instance creator
-const autoCompleteJS = new autoComplete({
-    data: {
-      src: async () => {
+  let searchBar = document.querySelector("#autoComplete");
+  const autoCompleteJS = new autoComplete({
+      data: {
+          src: async () => {
+          try {
+              // Loading placeholder text
+              document
+              .getElementById("autoComplete")
+              .setAttribute("placeholder", "Loading...");
+              // Fetch External Data Source
+              const source = await fetch(
+              "https://api.themoviedb.org/3/search/movie?api_key=5774e26c490bad12ed714f0dba3b87d3"
+              );
+              const data = await source.json();
+              // Post Loading placeholder text
+              document
+              .getElementById("autoComplete")
+              .setAttribute("placeholder", autoCompleteJS.placeHolder);
+              // Returns Fetched data
+              return data;
+          } catch (error) {
+              return error;
+          }
+          },
+          cache: true,
+      },
+      placeHolder: "Search for Movies!",
+      resultsList: {
+          element: (list, data) => {
+              // Create "No Results" message list element
+              const message = document.createElement("div");
+              // Add message text content
+              message.innerHTML = `<button id = "manualAdd">Add Manually</button>`;
+              // Add message list element to the list
+              list.prepend(message);
+          },
+          noResults: true,
+          maxResults: 15,
+          tabSelect: true
+      },
+      resultItem: {
+          element: (item, data) => {
+          // Modify Results Item Style
+          item.style = "display: flex; justify-content: space-between; margin-left: 0;";
+          // Modify Results Item Content
+          item.innerHTML = `
+          <span>
+              <img src="https://image.tmdb.org/t/p/original/${data.value.poster_path}" height="100">
+          </span>
+          <div style="width: 60%; padding-left: 10%; display: flex; align-items: center">
+              <span style="align-items: center; text-overflow: ellipsis; white-space: nowrap; overflow: hidden;">
+                  ${data.match}
+              </span>
+          </div>
+          <span style="display: flex; align-items: flex-end; font-size: 13px; font-weight: 100; text-transform: uppercase; color: rgba(0,0,0,.2);">
+              ${data.value.release_date ? data.value.release_date : 'xxxx-xx-xx'}
+          </span>`;
+          },
+          highlight: true
+      },
+      events: {
+          input: {
+          focus: () => {
+              if (autoCompleteJS.input.value.length) autoCompleteJS.start();
+          }
+          }
+      }
+  });
+
+  searchBar.oninput = function(){
+    console.log(this.value);
+    autoCompleteJS.data = {
+        src: async () => {
         try {
-          // Loading placeholder text
-          document
+            // Loading placeholder text
+            document
             .getElementById("autoComplete")
             .setAttribute("placeholder", "Loading...");
-          // Fetch External Data Source
-          const source = await fetch(
-            "https://tarekraafat.github.io/autoComplete.js/demo/db/generic.json"
-          );
-          const data = await source.json();
-          // Post Loading placeholder text
-          document
+            // Fetch External Data Source
+            const source = await fetch(
+            `https://api.themoviedb.org/3/search/movie?api_key=5774e26c490bad12ed714f0dba3b87d3&query=${this.value}`
+            );
+            const data = await source.json();
+            // Post Loading placeholder text
+            document
             .getElementById("autoComplete")
             .setAttribute("placeholder", autoCompleteJS.placeHolder);
-          // Returns Fetched data
-          return data;
+            // Returns Fetched data
+            return data.results;
         } catch (error) {
-          return error;
+            return error;
         }
-      },
-      keys: ["food", "cities", "animals"],
-      cache: true,
-      filter: (list) => {
-        // Filter duplicates
-        // incase of multiple data keys usage
-        const filteredResults = Array.from(
-          new Set(list.map((value) => value.match))
-        ).map((food) => {
-          return list.find((value) => value.match === food);
-        });
-  
-        return filteredResults;
-      }
-    },
-    placeHolder: "Search for Food & Drinks!",
-    resultsList: {
-      element: (list, data) => {
-        const info = document.createElement("p");
-        if (data.results.length > 0) {
-          info.innerHTML = `Displaying <strong>${data.results.length}</strong> out of <strong>${data.matches.length}</strong> results`;
-        } else {
-          info.innerHTML = `Found <strong>${data.matches.length}</strong> matching results for <strong>"${data.query}"</strong>`;
-        }
-        list.prepend(info);
-      },
-      noResults: true,
-      maxResults: 15,
-      tabSelect: true
-    },
-    resultItem: {
-      element: (item, data) => {
-        // Modify Results Item Style
-        item.style = "display: flex; justify-content: space-between;";
-        // Modify Results Item Content
-        item.innerHTML = `
-        <span style="text-overflow: ellipsis; white-space: nowrap; overflow: hidden;">
-          ${data.match}
-        </span>
-        <span style="display: flex; align-items: center; font-size: 13px; font-weight: 100; text-transform: uppercase; color: rgba(0,0,0,.2);">
-          ${data.key}
-        </span>`;
-      },
-      highlight: true
-    },
-    events: {
-      input: {
-        focus: () => {
-          if (autoCompleteJS.input.value.length) autoCompleteJS.start();
-        }
-      }
-    }
-  });
+        },
+        keys: ["title"]
+    };
+}
 
 
   // user selection leads to add new movie thing
